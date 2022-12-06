eb87450940d43989d49556675f8eaf093ad2413f
KaloyanKoleve <kaloqnkolev555@gmail.com>
2022-12-06 11:29:51 +0200

    changess

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let prices = document.querySelectorAll('.hot');
+  prices.forEach(element => {
+    element.innerText = element.innerText + "🔥"
+  });
 });
